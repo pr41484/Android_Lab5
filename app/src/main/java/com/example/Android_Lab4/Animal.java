f72ae1006bf5a42890a48ab828ceb3cd3125563a
pr41484 <63155216+pr41484@users.noreply.github.com>
2020-06-16 13:46:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/Android_Lab4/Animal.java
@@ -0,0 +1,48 @@
+package com.example.android_lab04;
+
+import java.io.Serializable;
+
+public class Animal implements Serializable {
+    private int _id;
+    private String gatunek;
+    private String kolor;
+    private Float wielkosc;
+    private String opis;
+
+    public Animal() {
+    }
+
+    public Animal(String gatunek, String kolor, Float wielkosc, String opis) {
+        super();
+        this.gatunek = gatunek;
+        this.kolor = kolor;
+        this.wielkosc = wielkosc;
+        this.opis = opis;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Zwierze: [id=" + _id + ", gatunek=" + gatunek + ", kolor=" + kolor
+                + ", wielkosc=" + wielkosc + " ]";
+    }
+
+    public int get_id() {
+        return _id;
+    }
+    public String getGatunek() {
+        return gatunek;
+    }
+    public String getKolor() {
+        return kolor;
+    }
+    public Float getWielkosc() {
+        return wielkosc;
+    }
+    public String getOpis() {
+        return opis;
+    }
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+}
